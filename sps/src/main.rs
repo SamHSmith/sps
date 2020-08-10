2c3a991a539e65f92939fc85de94261f80e343cd
Sam Smith <sam.henning.smith@protonmail.com>
2020-08-10 19:30:17 +0200

    Added ipfs key gen, and pkg toml parsing

--- sps/src/main.rs
+++ sps/src/main.rs
@@ -24,9 +24,9 @@ enum SubCommand {
 
 #[derive(Clap)]
 struct Add {
-    /// Repository to add this package to.
-    #[clap()]
     path_to_repo: PathBuf,
+    #[clap(default_value = ".")]
+    path_to_proj: PathBuf,
 }
 #[derive(Clap)]
 struct New {
@@ -40,11 +40,103 @@ struct Delete {
     #[clap()]
     path_to_repo: PathBuf,
 }
+use semver::Version;
+#[derive(Debug)]
+struct PackageMetaData {
+    name: String,
+    version: Version,
+    description: String,
+}
+#[derive(Debug)]
+struct ProjectConfig {
+    flags: Vec<String>,
+    archs: Vec<String>,
+    enums: Vec<(String, Vec<String>)>,
+}
 
 fn main() {
     let opts: Opts = Opts::parse();
 
     match opts.subcmd {
+        SubCommand::Add(a) => {
+            assert!(a.path_to_proj.is_dir());
+            assert!(a.path_to_repo.is_dir());
+            use std::fs::*;
+            use toml::Value;
+            let mut proj_meta = a.path_to_proj.clone();
+            proj_meta.push("meta.toml");
+            let proj_meta = read_to_string(&proj_meta)
+                .unwrap()
+                .parse::<Value>()
+                .unwrap();
+            let metadata = PackageMetaData {
+                name: proj_meta["name"].as_str().unwrap().to_string(),
+                version: Version::parse(proj_meta["version"].as_str().unwrap())
+                    .expect("Invalid version string"),
+                description: proj_meta["description"]
+                    .as_str()
+                    .unwrap_or(proj_meta["name"].as_str().unwrap())
+                    .to_string(),
+            };
+            println!("{:?}", metadata);
+
+            let mut proj_conf = a.path_to_proj.clone();
+            proj_conf.push("config.toml");
+            let proj_conf = read_to_string(&proj_conf)
+                .unwrap()
+                .parse::<Value>()
+                .unwrap();
+            assert!(
+                proj_conf.as_table().unwrap().contains_key("enums"),
+                "Found no enums = [] in config.toml"
+            );
+            assert!(
+                proj_conf.as_table().unwrap().contains_key("archs"),
+                "Found no archs = [] in config.toml"
+            );
+            assert!(
+                proj_conf.as_table().unwrap().contains_key("flags"),
+                "Found no flags = [] in config.toml"
+            );
+            let mut enums = Vec::new();
+            for e in proj_conf["enums"]
+                .as_array()
+                .expect("Found no enums = [] in config.toml")
+                .iter()
+                .map(|x| x.as_str().unwrap().to_string())
+            {
+                assert!(
+                    proj_conf.as_table().unwrap().contains_key(&e),
+                    format!("There was no enum name {} in config.toml", &e),
+                );
+                enums.push((
+                    e.clone(),
+                    proj_conf[&e]
+                        .as_array()
+                        .unwrap()
+                        .iter()
+                        .map(|x| x.as_str().unwrap().to_string())
+                        .collect(),
+                ));
+            }
+
+            let configdata = ProjectConfig {
+                flags: proj_conf["flags"]
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|x| x.as_str().unwrap().to_string())
+                    .collect(),
+                archs: proj_conf["archs"]
+                    .as_array()
+                    .expect("Found no archs = [] in config.toml")
+                    .iter()
+                    .map(|x| x.as_str().unwrap().to_string())
+                    .collect(),
+                enums: enums,
+            };
+            println!("{:?}", configdata);
+        }
         SubCommand::New(n) => {
             if n.path_to_repo.exists() {
                 assert!(false, "Big bad. It exists");
@@ -74,7 +166,7 @@ fn main() {
             write(
                 &path,
                 &format!(
-"name = \"{}\"
+                    "name = \"{}\"
 key = \"{}\"
 address = \"{}\"
 ",
@@ -112,7 +204,7 @@ fn ipfs_key_gen(key_name: &str) -> String {
     use std::io::Write;
     std::io::stderr().write_all(&output.stderr).unwrap();
     assert!(output.status.success());
-    output.stdout.truncate(output.stdout.len()-1);
+    output.stdout.truncate(output.stdout.len() - 1);
     String::from_utf8(output.stdout).unwrap()
 }
 
@@ -126,4 +218,3 @@ fn ipfs_key_rm(key_name: &str) {
     std::io::stderr().write_all(&output.stderr).unwrap();
     assert!(output.status.success());
 }
-
