718bbf4d8c9194b309ae31625f302ed5a5ff54dd
Sam Smith <sam.henning.smith@protonmail.com>
2020-08-16 00:38:10 +0200

    D is strange. Yet awfully familiar.
    The code I'm writing feels very GC'y. Doesn't matter really. These are the tools I have, and I will use them.
    It just honestly feels like I've taken a step back. Writing in the style of a younger self. Well, my younger self
    was too dump for switch statements. And he couldn't design systems the way I can now. So it'll probably be alright.

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-cd sps && cargo build --target=x86_64-unknown-linux-musl && cd ..
+cd sps.rust && cargo build --target=x86_64-unknown-linux-musl && cd ..
+cd sps && dub build --compiler=ldc2 && cd ..
+
+
+
