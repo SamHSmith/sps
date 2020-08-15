8e32c017b90d13f11eb77e67b2b99ea82e5d2206
Sam Smith <sam.henning.smith@protonmail.com>
2020-08-16 00:44:21 +0200

    I didn't delete all the code with dfmt this time.

--- sps/source/main.d
+++ sps/source/main.d
@@ -1,25 +1,29 @@
 import std.stdio;
 
-void main(string[] args) {
-writeln("Welcome to the SPS package manager");
-if (args.length <= 1) {
-args ~= "";
+void main(string[] args)
+{
+    writeln("Welcome to the SPS package manager");
+    if (args.length <= 1)
+    {
+        args ~= "";
+    }
+
+    switch (args[1])
+    {
+    case "repo":
+        goto case "repository";
+
+    case "repository":
+        repository_cli(args);
+        break;
+
+    default:
+        writeln("Error, unrecognised argument ", args[1]);
+        break;
+    }
 }
 
-switch (args[1]) {
-case "repo":
-goto case "repository";
-
-case "repository":
-repository_cli(args);
-break;
-
-default:
-writeln("Error, unrecognised argument ", args[1]);
-break;
-}
-}
-
-void repository_cli(string[] args) {
-writeln("Welcome to the repository CLI");
+void repository_cli(string[] args)
+{
+    writeln("Welcome to the repository CLI");
 }
